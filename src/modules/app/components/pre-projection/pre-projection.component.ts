a6f70a5f9891cd23a5a1d518abd4db5b75582320
Pritam Kininge <kininge007@gmail.com>
2020-09-06 12:09:28 +0530

    Pre-processing

--- src/modules/app/components/pre-projection/pre-projection.component.ts
+++ src/modules/app/components/pre-projection/pre-projection.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pre-projection.component.html',
   styleUrls: ['./pre-projection.component.scss']
 })
-export class PreProjectionComponent implements OnInit {
+export class PreProjectionComponent implements OnInit 
+{
+  public path: string= undefined;
+  public width: number= 80;
+  public height: number= 80;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  pageStatus()
+  {
+    if(this.path)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
+  collectData()
+  {
+    this.path= undefined;
+    this.width= 80;
+    this.height= 80;
+  }
 }
